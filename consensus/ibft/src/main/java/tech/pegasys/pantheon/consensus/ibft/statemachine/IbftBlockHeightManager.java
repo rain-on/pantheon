2f3044da9583db459a5901c2f36c113e06395e9c
tmohay <trent.mohay@consensys.net>
2019-02-01 15:11:32 +1100

    Update RoundChangeManager to use flattened message

--- consensus/ibft/src/main/java/tech/pegasys/pantheon/consensus/ibft/statemachine/IbftBlockHeightManager.java
+++ consensus/ibft/src/main/java/tech/pegasys/pantheon/consensus/ibft/statemachine/IbftBlockHeightManager.java
@@ -205,7 +205,7 @@ public class IbftBlockHeightManager implements BlockHeightManager {
     }
 
     final Optional<RoundChangeCertificate> result =
-        roundChangeManager.appendRoundChangeMessage(message.getSignedPayload());
+        roundChangeManager.appendRoundChangeMessage(message);
     if (result.isPresent()) {
       if (messageAge == FUTURE_ROUND) {
         startNewRound(targetRound.getRoundNumber());
